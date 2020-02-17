56e9b8bc15c6dd88166c97d4435eb672582cc63f
g_leu <ginaleuty15@gmail.com>
2020-02-17 00:16:31 -0600

    initial commit

---
+++ src/main/java/com/gleu/scheduler/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.gleu.scheduler.repository;
+
+import com.gleu.scheduler.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
